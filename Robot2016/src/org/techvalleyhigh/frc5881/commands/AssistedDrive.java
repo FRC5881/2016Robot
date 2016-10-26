9c0e27660b7c622738c24aae943ec06df410b183
ksharpe <ksharpe@techvalleyhigh.org>
2016-10-26 16:12:51 -0400

    Added variables and instances for left and right encoders

--- Robot2016/src/org/techvalleyhigh/frc5881/commands/AssistedDrive.java
+++ Robot2016/src/org/techvalleyhigh/frc5881/commands/AssistedDrive.java
@@ -113,7 +113,6 @@ public class AssistedDrive extends Command {
     		System.out.println("[TURN] Offset " + degreeOffset + " Drive(0, "
     				+ direction * speed + ")");
 
-    		Robot.driveControl.rawDrive(drivePower/3f, direction * speed);
     	} else {
     		// If on bearing, start timer and drive bot...
     		timer.start();
